080aceebf46c8482395dd097e6a4fc4bd07f99ea
Aaron Eline <aeline@umd.edu>
2022-05-03 11:04:41 -0400

    WIP

--- Horkos.rkt
+++ Horkos.rkt
@@ -1,12 +1,13 @@
 #lang racket
 (require "adi/adi.rkt") ;; for once we put adi as a subfolder
-
+(require "deserialize.rkt")
 ;;Given an outlaw program converts to an outlaw program with pledges inserted
 ;; this can then be compiled as usual
 
 (define (horkos-main p)
-  (let* ([ex (unparse (parse p))]
+  (let* ([ex (unfold-letrec (parse p))]
          [le (label-exp ex)]
          [s (run-algo le)])
     (pledge-insert le s)))
-            
+
+
